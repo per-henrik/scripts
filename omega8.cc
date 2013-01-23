d08060a490bb571299d31150790dfaf998abe77d
Per Oja <per@oja.no>
2013-01-23 19:09:32 +0100

    added mindcrack

---
+++ omega8.cc
@@ -0,0 +1,2 @@
+#! /bin/bash
+ssh o4774969007.ftp@o4774969007.v81.lon.host8.biz
---
+++ test.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+echo "Do you wish to install this program?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) make install; break;;
+        No ) exit;;
+    esac
+done
