facbbc7c6f75f092ff4ef11a0e02b70797f8c571
Per Oja <per@oja.no>
2013-04-21 17:59:35 +0200

    drushcleanup added

---
+++ drupal_cleaner.sh
@@ -0,0 +1,13 @@
+drush -y en devel
+drush -y en devel_generate
+drush -y en admin_menu
+drush -y dis toolbar
+drush -y en ns_article
+drush -y en views_ui
+drush -y en panels_ipe
+drush -y en panelizer
+drush -y en rules_admin
+drush -y en field_ui
+drush -y ns_footer_content
+drush -y contextual
+drush cc all
