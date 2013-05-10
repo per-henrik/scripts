36bcdda7324d480099a92e58089f69b9aef61505
Per Oja <per@oja.no>
2013-05-10 14:42:41 +0200

    bugfix downloads_2

--- drupal_cleaner.sh
+++ drupal_cleaner.sh
@@ -1,11 +1,10 @@
 drush -n dl devel
 drush -n dl admin_menu
-drush -n dl views_ui
-drush -n dl panels_ipe
+drush -n dl views
+drush -n dl panels
 drush -n dl panelizer
-drush -n dl rules_admin
+drush -n dl rules
 drush -n dl field_ui
-drush -n dl ns_footer_content
 drush -n dl contextual-7.x-2.0-alpha1
 drush -y en devel
 drush -y en devel_generate
@@ -16,6 +15,5 @@ drush -y en panels_ipe
 drush -y en panelizer
 drush -y en rules_admin
 drush -y en field_ui
-drush -y en ns_footer_content
 drush -y en contextual
 drush cc all
