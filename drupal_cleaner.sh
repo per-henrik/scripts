a7cf0651b04837637843cb23bc32b7bde94d1efa
Per Oja <per@oja.no>
2013-12-12 13:15:41 +0100

    Removed ds from drupal_cleaner.sh

--- drupal_cleaner.sh
+++ drupal_cleaner.sh
@@ -7,7 +7,7 @@ drush -n dl rules
 drush -n dl i18n
 drush -n dl entityreference
 drush -n dl module_filter
-drush -n dl ds
+# drush -n dl ds
 drush -n dl devel_image_provider-7.x-1.x-dev
 drush -y en devel
 drush -y en devel_generate
@@ -21,7 +21,7 @@ drush -y en field_ui
 drush -y en module_filter
 # drush -y en contextual
 drush -y en devel_image_provider
-drush -y en ds ds_ui ds_format ds_search ds_extras
+# drush -y en ds ds_ui ds_format ds_search ds_extras
 drush -y en entityreference
 drush -y vset theme_default garland
 drush cc all
