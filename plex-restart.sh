adc6812f0751ad0d8240d92fa2db2f79d6a76914
Per Oja <per@oja.no>
2018-09-05 06:42:55 +0200

    clean unused files

--- plex-restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-killall Plex
-killall Plex\ Media\ Server
-open /Applications/Plex\ Media\ Server.app/
-open /Applications/Plex.app
--- terve.localdev.wunder.io
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh root@terve.localdev.wunder.io
